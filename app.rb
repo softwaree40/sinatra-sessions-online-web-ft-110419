9397e09fdc9b0f44d64198f105d21d0d55c928e2
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-15 18:30:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,6 +22,7 @@ class App < Sinatra::Base
 
   get '/set' do
     binding.pry
+    session[:foo] = "hello"
     # set the :foo key of the session hash equal to 'hello' here!
     if session[:foo] == 'hello'
       redirect '/fetch'
